145175539f5ada888b252fb0ba69e8c00b75d3fc
GrillazsMozi <131274739+grillazsmozi@users.noreply.github.com>
2024-02-19 18:07:53 +0100

    a

---
+++ 20foglalkozas/script.js
@@ -0,0 +1,18 @@
+let a = 2 + 2
+
+switch (a) {
+    case 3:
+        alert('Túl kicsi!')
+        break
+
+    case 4:
+        alert('Pontosan!')
+        break
+
+    case 5:
+        alert('Túl nagy!')
+        break
+    
+    default:
+        alert('Nem tudom értelmezi...')
+}

3e81058c44fb903ef4324563abc20835b27c4cb6
GrillazsMozi <131274739+grillazsmozi@users.noreply.github.com>
2024-02-19 18:25:20 +0100

    a

--- 20foglalkozas/script.js
+++ 20foglalkozas/script.js
@@ -1,9 +1,12 @@
-let browser = prompt('Mi a böngésződ?')
+let a = +prompt('a?', '')
 
-if (browser === "Edge") {
-    alert("You've got the Edge!")
-} else if (browser === "Chrome" || browser === "Firefox" || browser === "Safari" || browser === "Opera") {
-    alert('Okay, we support these browsers too.')
-} else {
-    alert('We hope that this page looks okay!')
-}
+switch (a) {
+    case 0:
+        alert(0)
+    case 1:
+        alert(1)
+    case 2:
+        alert("2,3")
+    case 3:
+        alert("2,3")
+} 
